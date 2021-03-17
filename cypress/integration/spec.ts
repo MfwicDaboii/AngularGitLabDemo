5450592ed40538db2e95c748f7efc4302a556f27
joshua <379389>
2021-03-17 09:30:32 +0100

    Update Cypress & Sonarqube

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,5 +1,5 @@
 it('loads examples', () => {
   const baseURL: string = 'http://localhost:4200/'
   cy.visit(baseURL);
-  cy.contains('Replace me with something relevant');
+  cy.contains('Resources');
 });
